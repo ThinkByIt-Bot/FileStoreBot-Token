85d0c115f2ddb90b7eb658ecb22357dc8bcaac85
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2023-11-22 17:24:49 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,12 +1,17 @@
 import motor.motor_asyncio
-import pymongo, os
 from config import DB_URI, DB_NAME
-
 
 dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
 database = dbclient[DB_NAME]
 
 user_data = database['users']
+
+default_verify = {
+    'is_verified': False,
+    'verified_time': 0,
+    'verify_token': "",
+    'link': ""
+}
 
 def new_user(id):
     return {
@@ -27,6 +32,15 @@ async def add_user(user_id: int):
     user = new_user(user_id)
     await user_data.insert_one(user)
     return
+
+async def get_verify_status(user_id):
+    user = await user_data.find_one({'_id': user_id})
+    if user:
+        return user.get('verify_status', default_verify)
+    return default_verify
+
+async def update_verify_status(user_id, verify):
+    await user_data.update_one({'_id': user_id}, {'$set': {'verify_status': verify}})
 
 async def full_userbase():
     user_docs = await user_data.find()
