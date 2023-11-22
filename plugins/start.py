2e3825733a32bce0e2f82def54a13f05e5bdf4e6
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2023-11-23 00:58:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -11,7 +11,7 @@ from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from bot import Bot
-from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION,, IS_VERIFY, VERIFY_EXPIRE, SHORTLINK_API, SHORTLINK_URL, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, TUT_VID
+from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, IS_VERIFY, VERIFY_EXPIRE, SHORTLINK_API, SHORTLINK_URL, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, TUT_VID
 from helper_func import subscribed, encode, decode, get_messages, get_shortlink, get_verify_status, update_verify_status
 from database.database import add_user, del_user, full_userbase, present_user
 from shortzy import Shortzy
