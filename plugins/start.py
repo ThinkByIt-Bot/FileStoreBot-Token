99a98ad86af755ffef3f730d7bd69e0a31ded3a0
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2023-11-24 01:42:22 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -113,9 +113,10 @@ async def start_command(client: Client, message: Message):
 
     elif verify_status['is_verified']:
         reply_markup = InlineKeyboardMarkup(
-            [InlineKeyboardButton("😊 About Me", callback_data="about"),
-             InlineKeyboardButton("🔒 Close", callback_data="close")]
+            [[InlineKeyboardButton("😊 About Me", callback_data="about"),
+              InlineKeyboardButton("🔒 Close", callback_data="close")]]
         )
+
         await message.reply_text(
             text=START_MSG.format(
                 first=message.from_user.first_name,
