999683e3967a33189c863fb3133b8e51c5c6660f
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2023-11-24 01:15:08 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -121,7 +121,7 @@ async def start_command(client: Client, message: Message):
                 [InlineKeyboardButton("Click here", url=link)],
                 [InlineKeyboardButton('🗳 Tutorial 🗳', url="www.google.com")]
             ]
-            await message.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: {get_readable_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for 24 hours after passing the ad.", reply_markup=InlineKeyboardMarkup(btn), protect_content=True)
+            await message.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for 24 hours after passing the ad.", reply_markup=InlineKeyboardMarkup(btn), protect_content=True)
 
         else:
             reply_markup = InlineKeyboardMarkup(
