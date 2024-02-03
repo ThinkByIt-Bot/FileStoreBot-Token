a77c401896bcb80e412efd00202b441107d0e188
kayyum khan <152464768+onekayyum@users.noreply.github.com>
2024-02-03 22:48:06 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/isaiminiprime_admin'>Click here</a>\n○ Channel : <a href='https://t.me/isaimini_updates'>@CodeXBotz</a>\n○ Support Group : <a href='https://t.me/isaiminiprime_support'>@CodeXBotzSupport</a></b>",
+            text = f"<b>○ Creator : <a href='t.me/onekayyum : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Update Channel : <a href='https://t.me/devkayyum'>Click here</a>\n○ Group : <a href='https://t.me/pervchatroom'></a>\n○ Support Group : <a href='https://t.me/Pervchatroom'>Support</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
