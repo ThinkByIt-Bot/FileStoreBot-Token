5520d370c08d3481a949c9c1c074641912b8bedb
YuuichiSama <142111069+YuuichiSama@users.noreply.github.com>
2024-05-12 17:44:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,9 +33,9 @@ DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "0b587e3b234144025200974459d559ee2f868fe7")
-VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
+VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 85400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID","Alina_Database/2")
+TUT_VID = os.environ.get("TUT_VID","")
 
 
 #force sub channel id, if you want enable force sub
